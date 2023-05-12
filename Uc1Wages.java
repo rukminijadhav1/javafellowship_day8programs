5d28703d35609bb8eb73acc990f3c0ca2188d60d
Rukmini Jadhav <rukminibiradar7@gmail.com>
2023-05-13 01:11:15 +0530

    added programs

---
+++ Uc1Wages.java
@@ -0,0 +1,16 @@
+package day8;
+
+public class Uc1Wages {
+	public static int isFullTime=1;
+    private static int empCheck=(int) (Math.floor(Math.random() * 10) % 2);
+    public static void employeeWage(){
+        if (empCheck==isFullTime)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
+    }
+    public static void main(String[ ] args){
+        employeeWage();
+    }
+
+}
